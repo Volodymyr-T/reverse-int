2f735b6d1fdc331b1840fe34361c6549a214e046
Volodymyr Tieliezhkin <57314700+Volodymyr-T@users.noreply.github.com>
2022-01-05 22:39:50 +0100

    feat: write function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  return parseInt (         // 6. Return result converted to a Number
+            Math.abs(n)     // 1. Get absolute value of n
+            .toString()     // 2. Convert a Number to a String
+            .split('')      // 3. Convert a String to an Array of chars
+            .reverse()      // 4. Cast a reverse method to reverse an Array
+            .join('')       // 5. Join an Array to a String
+  );
 }
